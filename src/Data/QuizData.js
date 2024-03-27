641783241db20c1f2f9a62038d7f044f962a7cbf
Milan Charan <milancharan4@gmail.com>
2024-03-27 16:44:32 +0530

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+            {
+                question: "Which of these is NOT a relational or logical operator?",
+                options:[ "=", "||", "==", "!="],
+                answer: 1
+            },
+            {
+                question: "The #include <stdio.h> is a ______________.",
+                options:["Inclusion directive", "File inclusion directive", "Preprocessor directive", "None of the above"],
+                answer: 1
+            },
+            {
+                question: "The correct format of declaring a function is:",
+                options: [" type_of_return name_of_function (argument type);", "type_of_return name_of_function (argument type){}", "type_of_return (argument type) name_of_function;", "all of the above"],
+                answer: 1
+            },
+            {
+                question: "Comment on the C statement given below: \n int (*p)[5];",
+                options: ["A ragged array", "An array “p” of pointers", "A pointer “p” to an array", "none of the above"],
+                answer:3
+            },
+        ];
